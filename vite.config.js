df3eb88a499bacf887d41f10f3e2e2af35a56433
mike <mikeeleman@gmail.com>
2024-11-19 00:07:25 +0100

    deploy

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
         symfonyPlugin(),
     ],
     build: {
+        outDir: "public/build",
         rollupOptions: {
             input: {
                 app: "./assets/js/app.js"
